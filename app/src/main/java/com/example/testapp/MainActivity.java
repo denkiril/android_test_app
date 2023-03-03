84b84d7c47be9631114864878b715feba50f2568
Denis Kirilyuk <d.kirilyuk@stork.ru>
2023-03-03 19:39:49 +0300

    init

---
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
+
+        textView.setText("App version: " + BuildConfig.VERSION_NAME);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("click!");
+            }
+        });
+    }
+}
